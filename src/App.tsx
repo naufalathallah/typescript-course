b13f33c88b152d2eb517c2f6f146c8f36982a656
Naufal A <spealdm6@gmail.com>
2024-07-07 21:06:17 +0700

    [36]: header components

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,14 @@
 import "./App.css";
 import CourseGoal from "./components/CourseGoal";
+import goalsImg from "./assets/goals.jpg";
+import Header from "./components/Header";
 
 function App() {
   return (
     <main>
+      <Header image={{ src: goalsImg, alt: "A list of goals" }}>
+        <h1>Heya children h1</h1>
+      </Header>
       <CourseGoal title="Tes React">
         <p>Ini children</p>
       </CourseGoal>
