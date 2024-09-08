82108b370cc2d020f60d95b5732809902ecf8143
Naufal A <spealdm6@gmail.com>
2024-09-08 07:31:36 +0700

    feat: use state

--- src/App.tsx
+++ src/App.tsx
@@ -2,16 +2,43 @@ import "./App.css";
 import CourseGoal from "./components/CourseGoal";
 import goalsImg from "./assets/goals.jpg";
 import Header from "./components/Header";
+import { useState } from "react";
+
+type CourseGoal = {
+  title: string;
+  description: string;
+  id: number;
+};
 
 function App() {
+  const [goals, setGoals] = useState<CourseGoal[]>([]);
+
+  function handleAddGoal() {
+    setGoals((prevGoals) => {
+      const newGoal: CourseGoal = {
+        id: Math.random(),
+        title: "Ini judul",
+        description: "Ini deskripsi",
+      };
+      return [...prevGoals, newGoal];
+    });
+  }
+
   return (
     <main>
       <Header image={{ src: goalsImg, alt: "A list of goals" }}>
         <h1>Heya children h1</h1>
       </Header>
-      <CourseGoal title="Tes React">
-        <p>Ini children</p>
-      </CourseGoal>
+      <button onClick={handleAddGoal}>Add Goal</button>
+      <ul>
+        {goals.map((goal) => (
+          <li key={goal.id}>
+            <CourseGoal title={goal.title}>
+              <p>{goal.description}</p>
+            </CourseGoal>
+          </li>
+        ))}
+      </ul>
     </main>
   );
 }
