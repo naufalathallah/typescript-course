c2161f1f37b62aa93c66d4a28cc71bd3d75c4a82
Naufal A <spealdm6@gmail.com>
2024-09-08 08:45:22 +0700

    fix: move to course goal list comp

--- src/App.tsx
+++ src/App.tsx
@@ -3,8 +3,9 @@ import CourseGoal from "./components/CourseGoal";
 import goalsImg from "./assets/goals.jpg";
 import Header from "./components/Header";
 import { useState } from "react";
+import CourseGoalList from "./components/CourseGoalList";
 
-type CourseGoal = {
+export type CourseGoal = {
   title: string;
   description: string;
   id: number;
@@ -30,15 +31,7 @@ function App() {
         <h1>Heya children h1</h1>
       </Header>
       <button onClick={handleAddGoal}>Add Goal</button>
-      <ul>
-        {goals.map((goal) => (
-          <li key={goal.id}>
-            <CourseGoal title={goal.title}>
-              <p>{goal.description}</p>
-            </CourseGoal>
-          </li>
-        ))}
-      </ul>
+      <CourseGoalList goals={goals} />
     </main>
   );
 }
