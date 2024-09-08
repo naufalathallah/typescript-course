b6af7d28fe7db3af9d6bb35266cfeea489b8e983
Naufal A <spealdm6@gmail.com>
2024-09-08 12:56:56 +0700

    fix: init warning box

--- src/components/CourseGoalList.tsx
+++ src/components/CourseGoalList.tsx
@@ -1,5 +1,7 @@
+import { ReactNode } from "react";
 import { CourseGoal as CGoal } from "../App";
 import CourseGoal from "./CourseGoal";
+import InfoBox from "./InfoBox";
 
 type CourseGoalListProps = {
   goals: CGoal[];
@@ -7,15 +9,27 @@ type CourseGoalListProps = {
 };
 
 export default function CourseGoalList({ goals, onDelete }: CourseGoalListProps) {
+  if (goals.length === 0) {
+    return <InfoBox mode="hint">Belum ada goals</InfoBox>;
+  }
+
+  let warningBox: ReactNode;
+  if (goals.length >= 4) {
+    warningBox = <InfoBox mode="warning">Kebanyakan goals pak</InfoBox>;
+  }
+
   return (
-    <ul>
-      {goals.map((goal) => (
-        <li key={goal.id}>
-          <CourseGoal id={goal.id} title={goal.title} onDelete={onDelete}>
-            <p>{goal.description}</p>
-          </CourseGoal>
-        </li>
-      ))}
-    </ul>
+    <>
+      {warningBox}
+      <ul>
+        {goals.map((goal) => (
+          <li key={goal.id}>
+            <CourseGoal id={goal.id} title={goal.title} onDelete={onDelete}>
+              <p>{goal.description}</p>
+            </CourseGoal>
+          </li>
+        ))}
+      </ul>
+    </>
   );
 }
