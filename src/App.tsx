cc5adbcd4cd7148f340fafdaec5520970323676d
Naufal A <spealdm6@gmail.com>
2024-10-02 16:12:34 +0700

    feat: form component

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,29 @@
+import { useRef } from "react";
 import "./App.css";
 import Button from "./components/Button";
 import Container from "./components/Container";
-import Input from "./components/input";
+import Input from "./components/Input";
+import Form, { FormHandle } from "./components/Form";
 
 function App() {
+  const input = useRef<HTMLInputElement>(null);
+  const customForm = useRef<FormHandle>(null);
+
+  function handleSave(data: unknown) {
+    const extractedData = data as { name: string; age: string };
+
+    if (!data || typeof data !== "object" || !("name" in data) || !("age" in data)) {
+      return;
+    }
+
+    console.log(extractedData);
+    customForm.current?.clear();
+  }
+
   return (
     <main>
-      <Input id="name" label="Your Name" type="text" />
-      <Input id="age" label=" Your Age" type="number" />
+      <Input id="name" label="Your Name" type="text" ref={input} />
+      {/* <Input id="age" label=" Your Age" type="number" /> */}
       <p>
         <Button>A Button</Button>
       </p>
@@ -15,6 +31,15 @@ function App() {
         <Button href="https://google.com">An Anchor</Button>
       </p>
       <Container as={Button}>Click Me</Container>
+
+      {/* ini form */}
+      <Form onSave={handleSave} ref={customForm}>
+        <Input type="text" label="Name" id="name" />
+        <Input type="number" label="Age" id="age" />
+        <p>
+          <Button>Save</Button>
+        </p>
+      </Form>
     </main>
   );
 }
