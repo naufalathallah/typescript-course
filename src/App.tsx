d3301ee93f27218080ff1849df98189fa49f6eed
Naufal A <spealdm6@gmail.com>
2024-10-13 12:42:18 +0700

    fix: init section 7

--- src/App.tsx
+++ src/App.tsx
@@ -1,66 +1,20 @@
-import { ReactNode, useEffect, useState } from "react";
-import { get } from "./util/http.ts";
-import BlogPosts, { BlogPost } from "./components/BlogPosts.tsx";
-import fetchingImg from "./assets/data-fetching.png";
-import { z } from "zod";
-
-const rawDataBlogPostSchema = z.object({
-  id: z.number(),
-  userId: z.number(),
-  title: z.string(),
-  body: z.string(),
-});
-
-const expectedResponseDataSchema = z.array(rawDataBlogPostSchema);
+import Header from './components/Header.tsx';
+import Shop from './components/Shop.tsx';
+import Product from './components/Product.tsx';
+import { DUMMY_PRODUCTS } from './dummy-products.ts';
 
 function App() {
-  const [fetchedPosts, setFetchedPosts] = useState<BlogPost[]>([]);
-  const [isFetching, setIsFetching] = useState<boolean>(false);
-  const [error, setError] = useState<string | null>(null);
-
-  useEffect(() => {
-    async function fetchPosts() {
-      setIsFetching(true);
-      try {
-        const data = await get("https://jsonplaceholder.typicode.com/posts", z.array(rawDataBlogPostSchema));
-        const parsedData = expectedResponseDataSchema.parse(data);
-        const blogPosts: BlogPost[] = parsedData.map((rawPost) => {
-          return {
-            id: rawPost.id,
-            title: rawPost.title,
-            text: rawPost.body,
-          };
-        });
-        setFetchedPosts(blogPosts);
-      } catch (error) {
-        if (error instanceof Error) {
-          setError(error.message);
-        }
-      }
-
-      setIsFetching(false);
-    }
-
-    fetchPosts();
-  }, []);
-
-  let content: ReactNode;
-
-  if (isFetching) {
-    content = <p id="loading-fallback">Loading...</p>;
-  } else if (error) {
-    content = <p>Error: {error}</p>;
-  } else if (fetchedPosts.length > 0) {
-    content = <BlogPosts posts={fetchedPosts} />;
-  } else {
-    content = <p>No posts found</p>;
-  }
-
   return (
-    <main>
-      <img src={fetchingImg} alt="abstract image" />
-      {content}
-    </main>
+    <>
+      <Header />
+      <Shop>
+        {DUMMY_PRODUCTS.map((product) => (
+          <li key={product.id}>
+            <Product {...product} />
+          </li>
+        ))}
+      </Shop>
+    </>
   );
 }
 
