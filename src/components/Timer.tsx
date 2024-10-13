1ccb307113e553bfe9561c1f255a82a7dacdbbce
Naufal A <spealdm6@gmail.com>
2024-10-13 09:29:14 +0700

    fix: set interval

--- src/components/Timer.tsx
+++ src/components/Timer.tsx
@@ -1,11 +1,32 @@
-import Container from './UI/Container.tsx';
-import { type Timer as TimerProps } from '../store/timers-context.tsx';
+import Container from "./UI/Container.tsx";
+import { type Timer as TimerProps } from "../store/timers-context.tsx";
+import { useEffect, useRef, useState } from "react";
 
 export default function Timer({ name, duration }: TimerProps) {
+  const interval = useRef<number | null>(null);
+  const [remainingTime, setRemainingTime] = useState(duration * 1000);
+  const formattedRemainingTime = (remainingTime / 1000).toFixed(2);
+
+  if (remainingTime <= 0 && interval.current) {
+    clearInterval(interval.current);
+  }
+
+  useEffect(() => {
+    const timer = setInterval(() => {
+      setRemainingTime((prevTime) => prevTime - 50);
+    }, 50);
+    interval.current = timer;
+
+    return () => clearInterval(timer);
+  }, []);
+
   return (
     <Container as="article">
       <h2>{name}</h2>
-      <p>{duration}</p>
+      <p>
+        <progress max={duration * 1000} value={remainingTime} />
+      </p>
+      <p>{formattedRemainingTime}</p>
     </Container>
   );
 }
